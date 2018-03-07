7f2add8030cb8615f7c9edd40b64fc23e38ee75a
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-07 22:44:42 +0600

    Create models, add to Admin

--- techpro/home/admin.py
+++ techpro/home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Photo
 
 # Register your models here.
+class Photo_Admin(admin.ModelAdmin):
+	list_display = ['category_name', 'project_name']
+
+	class Meta:
+		model = Photo
+
+admin.site.register(Photo, Photo_Admin)
--- techpro/home/models.py
+++ techpro/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+	category_name = models.CharField(max_length=200)
+	project_name = models.CharField(max_length=200)
+	width = models.IntegerField(default=0)
+	height = models.IntegerField(default=0)
+	image = models.ImageField(null=False, blank=False, width_field='width', height_field='height')
+	upload_time = models.DateTimeField(auto_now_add=True, auto_now=False)
+	update_time = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+	def __str__(self):
+		return self.project_name
+
+		class Meta:
+			ordering = ['-upload_time']
