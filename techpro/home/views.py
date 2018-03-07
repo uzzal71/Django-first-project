227210eed5e9b9746f76338d0fd6ac5109e69cef
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-07 21:50:38 +0600

    Create Templates index.html and view home pages.

--- techpro/home/views.py
+++ techpro/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('<h2>Welcome website home pages.</h2>')
+	return render(request, 'index.html')
